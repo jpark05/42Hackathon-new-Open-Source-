c313a1183d2a5e938b18e7095cab90b88549ef64
jpark05 <42.05.jpark@gmail.com>
2021-11-27 05:51:08 +0900

    major changes to graph and design
    add graph slide, slider, footer icons and link

--- new design/js/main.js
+++ new design/js/main.js
@@ -2,8 +2,39 @@ $(document).ready(function() {
     //Main page FullPage
 	$('#fullpage').fullpage({
 		//options here
+		afterLoad: function(origin, destination, direction){
+			//using index
+			console.log(destination.index);
+
+			if (destination.index == 1){
+				$('.fixed-nav').fadeOut();
+			}
+			else{
+				$('.fixed-nav').fadeIn();
+			}
+			$('.fixed-nav').find('a').removeClass('active');
+			$('.fixed-nav').find('a').eq(destination.index).addClass('active');
+		}
 	});
 
+	//PPT Page Swiper
+	const swiper = new Swiper('.swiper', {
+		// Optional parameters
+		direction: 'horizontal',
+		loop: true,
+	  
+		// If we need pagination
+		pagination: {
+		  el: '.swiper-pagination',
+		  clickable: true
+		},
+	  
+		// Navigation arrows
+		navigation: {
+		  nextEl: '.swiper-button-next',
+		  prevEl: '.swiper-button-prev',
+		}
+	  });
 
 
 });//document ready
